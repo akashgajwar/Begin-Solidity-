c88a9d76bf7e2e750a469409ed70f94a52f33be3
Akash Gajwar <75195665+akashgajwar@users.noreply.github.com>
2022-09-22 02:51:44 +0530

    Calculations

--- FD.sol
+++ FD.sol
@@ -5,29 +5,50 @@ pragma solidity =0.8.7;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract Bank is ERC20 { 
+
     constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
         _mint(msg.sender, 10000);
     } 
     event SENDER(address);
     
     uint256 B_rate = 11;
-    uint256 D_rate = 6 ;
+    uint256 D_rate = 6 ; 
 
-   struct Data {
+  
+    mapping (address => uint) public Data;
+
+    struct Record {
        uint256 amount;
        uint256 lastTime;
-   }
+    }
 
-    mapping (address => Data) public calc;
+    Record[] public datas; 
 
     function deposit(uint256 _amount) external {
         
         transfer( address(this), _amount);
-        // emit SENDER(address(this));
-    }
 
+        Record memory dataa;
+        dataa.amount = _amount;
+        dataa.lastTime = block.timestamp;
+
+        //  function create (address _add) public {
+        //     datas.push(Data(_amount, block.timestamp));
+        // }
+
+        //  Data ({
+        //         amount : _amount,
+        //         lastTime : block.timestamp
+        //     });
+    }      
+           
+                      
     function Lend(uint256 _amount) external {
         _transfer(address(this), msg.sender, _amount);
+
+    }
+
+    function  calc(address _add)public{
 
     }
 }
