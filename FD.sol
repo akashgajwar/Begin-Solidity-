3e24d7ba69bef61def46bee842f63de402950091
Akash Gajwar <75195665+akashgajwar@users.noreply.github.com>
2022-10-03 12:16:05 +0530

    Multi account handling fixed.

--- FD.sol
+++ FD.sol
@@ -19,49 +19,53 @@ contract Bank is ERC20 {
        uint256 amount;
        uint256 lastTime;
     }
-// Deposit//
+// Deposit//- multiple deposit amount update how ? 
 
     function deposit(uint256 _amount) external {
         
         transfer( address(this), _amount);
 
-        Data[msg.sender] = Record(_amount, block.timestamp);
+        uint256 total = calc();
 
-    }      
+        uint256 superAmount = _amount + total;
 
+        Data[msg.sender] = Record(superAmount, block.timestamp);
+
+    }        
  //withdraw// scale it for multiple people , person can withdraw any amout but it should be less than its deposit+interest, set amount, timestamp in struct after withdaral, 
 //  add events in contract //
 
-    function withdraw () public returns(uint256) {
+    function withdraw ( uint256 _amount) public returns(uint256) {
         
         uint256 total = calc();
+        Data[msg.sender].amount = total;
         uint256 balanceOfBank = balanceOf(address(this));
 
         if (balanceOfBank >=total){
-            _transfer(address(this), msg.sender, total);
+            _transfer(address(this), msg.sender, _amount);
         }else{
-            _mint(msg.sender, total - balanceOfBank);
-            _transfer(address(this), msg.sender, balanceOfBank);
+            _mint(address(this), (total - balanceOfBank));
+            _transfer(address(this), msg.sender, _amount);
         }
         
-        
-        Data[msg.sender].amount = 0;    
+        Data[msg.sender].amount = Data[msg.sender].amount - _amount ;    
+        Data[msg.sender].lastTime = block.timestamp;
 
         return total;
     }
 
-// Interest calculation//
+// Interest calculation//  interest cal krke add kro amount mai  ?
 
-    function  calc() public view returns(uint _total){
+    function calc() public view returns(uint256 _total){
 
         uint P = Data[msg.sender].amount;
-        uint T= (block.timestamp - (Data[msg.sender].lastTime)/1 seconds);
+        uint T= (block.timestamp - (Data[msg.sender].lastTime));
         
         uint SI = (P*T*rate)/100;
-
         uint total = P + SI;
-
+        
         return total;
 
     }
+
 }
