0c5174ada099eac43d33d6e11b794bd43e455f2c
Akash Gajwar <75195665+akashgajwar@users.noreply.github.com>
2022-10-22 02:48:12 +0530

    Code done

--- ERP721.sol
+++ ERP721.sol
@@ -23,6 +23,69 @@ contract ERC721 is IERC721{
 
     function balanceOf(address owner) external view returns(uint ){
         require(owner != address(0), "Address zero");
-        return
+        return _balanceOf[owner];
     }
+
+    function setApprovalForAll(address operator, bool approved)external{
+        isApprovedForAll[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator, approved);
+    }
+
+    function approve(address spender, uint id)external {
+        address owner = _ownerOf[id];
+        require(msg.sender == owner || isApprovedForAll[owner][msg.sender], "not authorized");
+        _approvals[id]= spender;
+
+        emit approval(owner, spender, id);
+    }
+
+    function getApproved(uint id)external view returns(address){
+        require(owner[id] != address(0), "token does not exists");
+        return _approvals[id];
+    }
+
+    function _isApprovedOwner(address owner, address spender, uint id) internal view returns (bool) {
+        require(spendr == owner || isApprovedForAll[owner][spender] || spender == _approvals[id]);
+
+    }
+
+    function transferFrom(address from, address to, uint id) public{
+        require(from == _ownerOf[id], "from is not the owner of id");
+        require(to != address(0), "transfer to address zero");
+        require(isApprovedForAll(from, msg.sender, id), "Not authorised");
+        _balanceOf[from]--;
+        _balanceOf[to]++;
+        _ownerOf[id] = to;
+        delete _approvals[id];
+
+        emit Transfer(from, to, id);
+    }
+    
+    // function safeTransferFrom(address from, address to, uint id) external {
+    //     transferFrom(from, to, id);
+    //     require(to.code.length ==0 || IERC721Receiver(to).onERC721Received(msg.sender, from, id) == 
+    //     IERC721Receiver.onERC721Received.selector, "unsafe recipent");
+    // }
+
+    function _mint(address to, uint id) external{
+        require(to != address(0), "mint to address zero");
+        require(_ownerOf[id] == address(0), "already minted");
+
+        _balanceOf[to]++;
+        _ownerOf[id] == to;
+
+        emit Transfer(address(0), to, id);
+    }
+}
+
+contract MyNFT is ERC721{
+    function mint(address to, uint id)external{
+        _mint(to, id);
+    
+    }
+
+    function Transfer(address from, address to, uint id) external{
+        transferFrom(from, to, id);
+    }
+    
 }
